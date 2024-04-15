be0817b7ffab507c1628397b2cf87de2fdc8a842
llaurabat91 <laura.battaglia@barcelonagse.eu>
2024-04-15 18:01:49 +0100

    start cleanup

--- frugal_flows/__init__.py
+++ frugal_flows/__init__.py
@@ -1,3 +1,6 @@
-from .myrun import add_one
+# from .myrun import add_one
 
-add_one(5)
+# add_one(5)
+from .flows import masked_independent_flow
+
+__all__ = ["masked_independent_flow"]
---
+++ frugal_flows/bijections/__init__.py
@@ -0,0 +1,8 @@
+"""Bijections from ``frugal_flows.bijections``."""
+
+
+from .masked_independent import MaskedIndependent
+
+__all__ = [
+    "MaskedIndependent",
+]
