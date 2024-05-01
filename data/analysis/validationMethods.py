Fix bugs in causal validation code

data/analysis/validationMethods.py
@@ -24,13 +24,18 @@ import rpy2.robjects.numpy2ri
 rpy2.robjects.numpy2ri.activate()
 from rpy2.robjects import r, pandas2ri
 from rpy2.robjects.vectors import StrVector
+import logging
+
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
 
 pandas2ri.activate()
 
 utils = importr('utils')
 utils = rpy2.robjects.packages.importr('utils')
 utils.chooseCRANmirror(ind=1) # select the first mirror in the list
-packnames = ('dbarts', 'grf')
+packnames = ('dbarts', 'grf', 'marginaleffects')
 
 names_to_install = [x for x in packnames if not rpy2.robjects.packages.isinstalled(x)]
 if len(names_to_install) > 0:
@@ -53,45 +58,48 @@ def matchit(outcome: str, treatment: str, data: pd.DataFrame, method: str = 'ful
     float: The average treatment effect (ATE) estimated by the matching.
 
     """
+    covariates = [col for col in data.columns if col not in [outcome, treatment]]
     if replace:
         replace = 'TRUE'
     else:
         replace = 'FALSE'
     data.to_csv('data.csv',index=False)
-    formula_cov = treatment+' ~ '
-    i = 0
-    for cov in data.columns:
-        if cov!=outcome and cov!=treatment:
-            if i!=0:
-                formula_cov += '+' 
-            formula_cov += str(cov)
-            i += 1
-    string = f"""
-    library('MatchIt')
+    matching_formula= treatment + ' ~ ' + ' + '.join(covariates)
+    fitting_formula= outcome + ' ~ ' + treatment + ' * (' + ' + '.join(covariates) + ')'
+
+    r_code_string = f"""
+    library(MatchIt)
+    library(marginaleffects)
+
     data <- read.csv('data.csv')
-    r <- matchit( {formula_cov},estimand="ATE", method = "{method}", data = data, replace = {replace})
-    matrix <- r$match.matrix[,]
-    names <- as.numeric(names(r$match.matrix[,]))
-    mtch <- data[as.numeric(names(r$match.matrix[,])),]
-    hh <- data[as.numeric(names(r$match.matrix[,])),'{outcome}']- data[as.numeric(r$match.matrix[,]),'{outcome}']
-    
-    data2 <- data
-    data2${treatment} <- 1 - data2${treatment}
-    r2 <- matchit( {formula_cov}, estimand="ATE", method = "{method}", data = data2, replace = {replace})
-    matrix2 <- r2$match.matrix[,]
-    names2 <- as.numeric(names(r2$match.matrix[,]))
-    mtch2 <- data2[as.numeric(names(r2$match.matrix[,])),]
-    hh2 <- data2[as.numeric(r2$match.matrix[,]),'{outcome}'] - data2[as.numeric(names(r2$match.matrix[,])),'{outcome}']
-    """#%( formula_cov,method,replace,outcome,outcome, treatment, treatment, formula_cov,method,replace,outcome,outcome)
-    
-    psnn = SignatureTranslatedAnonymousPackage(string, "powerpack")
-    match = psnn.mtch
-    match2 = psnn.mtch2
-    t_hat = pd.DataFrame(np.hstack((np.array(psnn.hh),np.array(psnn.hh2))),
-                         index=list(psnn.names.astype(int))+list(psnn.names2.astype(int)),
-                         columns=['CATE'])
-    ate = np.mean(t_hat['CATE'])
-    return ate
+
+    control_data <- data[data[['{treatment}']] == 0, ]
+    treatment_data <- data[data[['{treatment}']] == 1, ]
+
+    m.out <- matchit(
+        {matching_formula},
+        data = data,
+        method = "full",
+        estimand = "ATE",
+        distance = "glm",
+        replace = {replace}
+    )
+    matched_data <- match.data(m.out)
+
+    fit <- lm(as.formula({fitting_formula}), data = matched_data, weights = weights)
+    ate_fit <- avg_comparisons(fit,
+                variables = '{treatment}',
+                vcov = ~subclass,
+                newdata = subset(matched_data, X == 1),
+                wts = "weights")
+    ate <- ate_fit$estimate
+    ate_std <- ate_fit$std.error
+    """
+    psnn = SignatureTranslatedAnonymousPackage(r_code_string, "powerpack")
+    ate = psnn.ate
+    ate_std = psnn.ate_std
+    lower_conf, upper_conf = (ate_std * -1.96, ate_std * +1.96)
+    return ate, lower_conf, upper_conf
 
 
 def bart(outcome: str, treatment: str, data: pd.DataFrame) -> float:
@@ -137,9 +145,12 @@ def bart(outcome: str, treatment: str, data: pd.DataFrame) -> float:
     # Store the treatment effect in the CATE DataFrame
     cate_est['CATE'] = treatment_effect
     cate_est['avg.CATE'] = cate_est.mean(axis=1)
-    cate_est['std.CATE'] = cate_est.std(axis=1) 
-    ate_est = cate_est['avg.CATE'].mean(axis=0)
-    return ate_est
+    cate_est['std.CATE'] = cate_est.std(axis=1) # This doesn't really work, need to adjust the uncertainty 
+    ate= cate_est['avg.CATE'].mean(axis=0)
+
+    # lower_conf, upper_conf = (ate_std * -1.96, ate_std * +1.96)
+    lower_conf, upper_conf = (None, None) # Need to fix
+    return ate, lower_conf, upper_conf
 
 
 def causalforest(outcome: str, treatment: str, data: pd.DataFrame, n_splits: int = 5) -> float:
@@ -193,7 +204,11 @@ def causalforest(outcome: str, treatment: str, data: pd.DataFrame, n_splits: int
     cate_est['std.CATE'] = cate_est.std(axis=1)
 
     # Return the average of the average CATE estimates
-    return cate_est['avg.CATE'].mean()
+    ate = cate_est['avg.CATE'].mean()
+    ate_std = cate_est['std.CATE']
+    lower_conf, upper_conf = (ate_std * -1.96, ate_std * +1.96)
+    return ate, lower_conf, upper_conf
+
 
 def metalearner(outcome: str, treatment: str, data: pd.DataFrame, est: str = 'T', method: str = 'linear') -> float:
     """
@@ -209,25 +224,29 @@ def metalearner(outcome: str, treatment: str, data: pd.DataFrame, est: str = 'T'
     Returns:
     point (float): The estimated ATE by the meta-learner.
     """
+    # Choose models
     if method=='linear':
         models = RidgeCV()
         propensity_model = LogisticRegressionCV()
     if method=='GBR':
         models = GradientBoostingRegressor()
         propensity_model = GradientBoostingClassifier() 
+    
+    # Choose metalearner
     if est=='T':
-        T_learner = TLearner(models=models)
-        T_learner.fit(data[outcome], data[treatment], X=data.drop(columns=[outcome,treatment]))
-        point = T_learner.ate(X=data.drop(columns=[outcome,treatment]))
+        learner = TLearner(models=models)
+        learner.fit(data[outcome], data[treatment], X=data.drop(columns=[outcome,treatment]), inference='bootstrap') 
     elif est=='S':
-        S_learner = SLearner( overall_model=models)
-        S_learner.fit(data[outcome], data[treatment], X=data.drop(columns=[outcome,treatment]))
-        point = S_learner.ate(X=data.drop(columns=[outcome,treatment]))
+        learner = SLearner(overall_model=models)
+        learner.fit(data[outcome], data[treatment], X=data.drop(columns=[outcome,treatment]), inference='bootstrap') 
     elif est=='X':
-        X_learner = XLearner(models=models,propensity_model=propensity_model)
-        X_learner.fit(data[outcome], data[treatment], X=data.drop(columns=[outcome,treatment]))
-        point = X_learner.ate(X=data.drop(columns=[outcome,treatment]))
-    return point
+        learner = XLearner(models=models, propensity_model=propensity_model)
+        learner.fit(data[outcome], data[treatment], X=data.drop(columns=[outcome,treatment]), inference='bootstrap') 
+
+    results = learner.ate_inference(T0=0, T1=1,  X=data.drop(columns=[outcome, treatment]))
+    ate_point = results.mean_point
+    lower_conf, upper_conf = results.conf_int_mean()
+    return ate_point, (lower_conf, upper_conf)
 
 
 def dml(outcome: str, treatment: str, data: pd.DataFrame, method: str = 'GBR') -> float:
@@ -254,19 +273,22 @@ def dml(outcome: str, treatment: str, data: pd.DataFrame, method: str = 'GBR') -
             data[outcome],
             data[treatment],
             X=data.drop(columns=[outcome, treatment]),
-            W=data.drop(columns=[outcome, treatment])
+            W=data.drop(columns=[outcome, treatment]),
+            inference='bootstrap'
         )
-        point = est.ate(data.drop(columns=[outcome, treatment]), T0=0, T1=1)
     if method == 'linear':
         est = LinearDML(discrete_treatment=True)
         est.fit(
             data[outcome],
             data[treatment],
             X=data.drop(columns=[outcome, treatment]),
-            W=data.drop(columns=[outcome, treatment])
+            W=data.drop(columns=[outcome, treatment]),
+            inference='bootstrap'
         )
-        point = est.ate(data.drop(columns=[outcome, treatment]), T0=0, T1=1)
-    return point
+    results = est.ate_inference(T0=0, T1=1,  X=data.drop(columns=[outcome, treatment]))
+    ate_point = results.mean_point
+    lower_conf, upper_conf = results.conf_int_mean()
+    return ate_point, (lower_conf, upper_conf)
 
 
 def doubleRobust(outcome: str, treatment: str, data: pd.DataFrame) -> float:
@@ -286,11 +308,14 @@ def doubleRobust(outcome: str, treatment: str, data: pd.DataFrame) -> float:
         data[outcome],
         data[treatment],
         X = data.drop(columns=[outcome, treatment]),
-        W = None
+        W = None,
         #W=data.drop(columns=[outcome, treatment])
+        inference='bootstrap'
     )
-    point = est.ate(data.drop(columns=[outcome, treatment]), T0=0, T1=1)
-    return point
+    results = est.ate_inference(T0=0, T1=1,  X=data.drop(columns=[outcome, treatment]))
+    ate = results.mean_point
+    lower_conf, upper_conf = results.conf_int_mean()
+    return ate, lower_conf, upper_conf
 
 
 def tmle(outcome: str, treatment: str, data: pd.DataFrame) -> float:
@@ -313,7 +338,9 @@ def tmle(outcome: str, treatment: str, data: pd.DataFrame) -> float:
     tml.exposure_model(s)
     tml.outcome_model(s)
     tml.fit()
-    return tml.average_treatment_effect
+    ate = tml.average_treatment_effect
+    lower_conf, upper_conf = tml.average_treatment_effect_ci # 95th percentile
+    return ate, lower_conf, upper_conf
 
 
 def diff_means(outcome: str, treatment: str, data: pd.DataFrame) -> float:
@@ -328,7 +355,10 @@ def diff_means(outcome: str, treatment: str, data: pd.DataFrame) -> float:
     Returns:
     float: The difference in means.
     """
-    return data.loc[data[treatment] == 1, outcome].mean() - data.loc[data[treatment] == 0, outcome].mean()
+    ate = data.loc[data[treatment] == 1, outcome].mean() - data.loc[data[treatment] == 0, outcome].mean()
+    lower_conf = None
+    upper_conf = None
+    return ate, lower_conf, upper_conf
 
 
 def estimate_ate(outcome: str, treatment: str, data: pd.DataFrame) -> pd.DataFrame:
@@ -343,26 +373,109 @@ def estimate_ate(outcome: str, treatment: str, data: pd.DataFrame) -> pd.DataFra
     Returns:
     pd.DataFrame: A DataFrame containing the estimated ATEs for different methods.
     """
-    ate = {}
+    results = []
+    column_headers = ['ate', 'lower_conf', 'upper_conf']
+    method_names = pd.DataFrame({'method': [
+        # 'Diff. of Mean',
+        'Gradient Boosting Trees DML',
+        'Linear DML',
+        'Doubly Robust (Linear)',
+        'Linear T Learner',
+        'Linear S Learner',
+        'Linear X Learner',
+        'Gradient Boosting Trees T Learner',
+        'Gradient Boosting Trees S Learner',
+        'Gradient Boosting Trees X Learner',
+        #'Causal BART',
+        #'Causal Forest',
+        'Propensity Score Matching',
+        'TMLE'
+    ]})
+    #with open(os.devnull, "w") as f, contextlib.redirect_stdout(f):
+    #results.append(diff_means(outcome, treatment, data))
+    # Set up logging
+
+    # Replace the print statement with logging
+    logging.info(f"Fitting model: {method_names.iloc[0, 0]}")
+    results.append(dml(outcome, treatment, data, method='GBR'))
+    logging.info(f"Fitting model: {method_names.iloc[1, 0]}")
+    results.append(dml(outcome, treatment, data, method='linear'))
+    logging.info(f"Fitting model: {method_names.iloc[2, 0]}")
+    results.append(doubleRobust(outcome, treatment, data))
+    logging.info(f"Fitting model: {method_names.iloc[3, 0]}")
+    results.append(metalearner(outcome, treatment, data, est='T', method='linear'))
+    logging.info(f"Fitting model: {method_names.iloc[4, 0]}")
+    results.append(metalearner(outcome, treatment, data, est='S', method='linear'))
+    logging.info(f"Fitting model: {method_names.iloc[5, 0]}")
+    results.append(metalearner(outcome, treatment, data, est='X', method='linear'))
+    logging.info(f"Fitting model: {method_names.iloc[6, 0]}")
+    results.append(metalearner(outcome, treatment, data, est='T', method='GBR'))
+    logging.info(f"Fitting model: {method_names.iloc[7, 0]}")
+    results.append(metalearner(outcome, treatment, data, est='S', method='GBR'))
+    logging.info(f"Fitting model: {method_names.iloc[8, 0]}")
+    results.append(metalearner(outcome, treatment, data, est='X', method='GBR'))
+    # logging.info(f"Fitting model: {method_names.iloc[0, 0]}")
+    # results.append(bart(outcome, treatment, data))
+    # logging.info(f"Fitting model: {method_names.iloc[0, 0]}")
+    # results.append(causalforest(outcome, treatment, data))
+    logging.info(f"Fitting model: {method_names.iloc[9, 0]}")
+    results.append(matchit(outcome, treatment, data, method='full'))
+    logging.info(f"Fitting model: {method_names.iloc[10, 0]}")
     with open(os.devnull, "w") as f, contextlib.redirect_stdout(f):
-        ate['Diff. of Mean'] = diff_means(outcome, treatment, data)
-        ate['Gradient Boosting Trees DML'] = dml(outcome, treatment, data, method='GBR')
-        ate['Linear DML'] = dml(outcome, treatment, data, method='linear')
-        ate['Doubly Robust (Linear)'] = doubleRobust(outcome, treatment, data)
-        ate['Linear T Learner'] = metalearner(outcome, treatment, data, est='T', method='linear')
-        ate['Linear S Learner'] = metalearner(outcome, treatment, data, est='S', method='linear')
-        ate['Linear X Learner'] = metalearner(outcome, treatment, data, est='X', method='linear')
-        ate['Gradient Boosting Trees T Learner'] = metalearner(outcome, treatment, data, est='T', method='GBR')
-        ate['Gradient Boosting Trees S Learner'] = metalearner(outcome, treatment, data, est='S', method='GBR')
-        ate['Gradient Boosting Trees X Learner'] = metalearner(outcome, treatment, data, est='X', method='GBR')
-        ate['Causal BART'] = bart(outcome, treatment, data)
-        ate['Causal Forest'] = causalforest(outcome, treatment, data)
-        # ate['Propensity Score Matching'] = matchit(outcome, treatment, data, method='full')
-        ate['TMLE'] = tmle(outcome, treatment, data)
-        return pd.DataFrame.from_dict(ate, orient='index').T
+        results.append(tmle(outcome, treatment, data))
+    results_df = pd.DataFrame(results, columns=column_headers)
+    return pd.concat([method_names, results_df], axis=1)
 
 
-def bootstrap_ate_inference(outcome: str, treatment: str, data: pd.DataFrame, repeats: int, sample_frac: float, replace: bool) -> pd.DataFrame:
+def bootstrap_estimate_ate(outcome: str, treatment: str, data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Estimate the Average Treatment Effect (ATE) using various methods.
+
+    Parameters:
+    outcome (str): The name of the outcome variable.
+    treatment (str): The name of the treatment variable.
+    data (pd.DataFrame): The input data containing the outcome, treatment, and covariates.
+
+    Returns:
+    pd.DataFrame: A DataFrame containing the estimated ATEs for different methods.
+    """
+    results = []
+    column_headers = ['ate', 'lower_conf', 'upper_conf']
+    method_names = pd.DataFrame({'method': [
+        'Diff. of Mean',
+        #'Gradient Boosting Trees DML',
+        #'Linear DML',
+        #'Doubly Robust (Linear)',
+        #'Linear T Learner',
+        #'Linear S Learner',
+        #'Linear X Learner',
+        #'Gradient Boosting Trees T Learner',
+        #'Gradient Boosting Trees S Learner',
+        #'Gradient Boosting Trees X Learner',
+        'Causal BART',
+        'Causal Forest',
+        #'Propensity Score Matching',
+        #'TMLE'
+    ]})
+    with open(os.devnull, "w") as f, contextlib.redirect_stdout(f):
+        results.append(diff_means(outcome, treatment, data))
+        # results.append(dml(outcome, treatment, data, method='GBR'))
+        # results.append(dml(outcome, treatment, data, method='linear'))
+        # results.append(doubleRobust(outcome, treatment, data))
+        # results.append(metalearner(outcome, treatment, data, est='T', method='linear'))
+        # results.append(metalearner(outcome, treatment, data, est='S', method='linear'))
+        # results.append(metalearner(outcome, treatment, data, est='X', method='linear'))
+        # results.append(metalearner(outcome, treatment, data, est='T', method='GBR'))
+        # results.append(metalearner(outcome, treatment, data, est='S', method='GBR'))
+        # results.append(metalearner(outcome, treatment, data, est='X', method='GBR'))
+        results.append(bart(outcome, treatment, data))
+        results.append(causalforest(outcome, treatment, data))
+        #results.append(matchit(outcome, treatment, data, method='full'))
+        #results.append(tmle(outcome, treatment, data))
+    results_df = pd.DataFrame(results, columns=column_headers)
+    return pd.concat([method_names, results_df], axis=1)
+
+def run_model_fits(outcome: str, treatment: str, data: pd.DataFrame, repeats: int, sample_frac: float, replace: bool) -> pd.DataFrame:
     """
     Perform bootstrap inference for estimating the Average Treatment Effect (ATE).
 
@@ -375,9 +488,14 @@ def bootstrap_ate_inference(outcome: str, treatment: str, data: pd.DataFrame, re
     Returns:
     pd.DataFrame: A DataFrame containing the estimated ATEs for each bootstrap iteration.
     """
-    results = []
+    non_bootstrap_results_df = estimate_ate(outcome, treatment, data)
+    bootstrap_results = []
     for _ in tqdm.tqdm(range(repeats), leave=False):
         data_ = data.sample(frac=sample_frac, replace=replace)
-        ate_ = estimate_ate(outcome, treatment, data_.reset_index(drop=True))
-        results.append(ate_)
-    return pd.concat(results, axis=0)
+        ate_ = bootstrap_estimate_ate(outcome, treatment, data_.reset_index(drop=True))
+        bootstrap_results.append(ate_)
+    bootstrap_results_df = pd.concat(bootstrap_results, axis=0)
+    return {
+        'bootstrap_results': bootstrap_results_df,
+        'nonbootstrap_results': non_bootstrap_results_df
+    }