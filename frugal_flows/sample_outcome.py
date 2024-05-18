f191cb541b23fe64ff2478cf47055b2063a5c409
llaurabat91 <laura.battaglia@barcelonagse.eu>
2024-05-18 16:16:08 +0100

    allow sample_outcome to sample from u_yx and add checks

--- frugal_flows/sample_outcome.py
+++ frugal_flows/sample_outcome.py
@@ -4,71 +4,147 @@ import warnings
 import jax
 import jax.numpy as jnp
 import jax.random as jr
+from flowjax.bijections import (
+    Affine,
+    Invert,
+    Tanh,
+)
 from flowjax.bijections.bijection import AbstractBijection
-from flowjax.distributions import AbstractDistribution
+from flowjax.bijections.utils import Identity
+from flowjax.distributions import AbstractDistribution, _StandardUniform
 from jaxtyping import ArrayLike
 
-from frugal_flows.bijections import LocCond, UnivariateNormalCDF
+from frugal_flows.bijections import (
+    LocCond,
+    MaskedAutoregressiveFirstUniform,
+    UnivariateNormalCDF,
+)
 
 
 def sample_outcome(
-    frugal_flow: AbstractDistribution,
-    affine_to_standard: bool,
     key: jr.PRNGKey,
     n_samples: float,
     causal_model: str,
     causal_condition: ArrayLike | None = None,
+    frugal_flow: AbstractDistribution | None = None,
     causal_cdf: AbstractBijection | None = UnivariateNormalCDF,
     u_yx: ArrayLike | None = None,
     **treatment_kwargs: dict,
 ):
     valid_causal_models = ["logistic_regression", "causal_cdf", "location_translation"]
-    flow_dim = frugal_flow.shape[0]
 
-    if frugal_flow.cond_shape is None:
-        flow_fake_condition = None
-    else:
-        flow_fake_condition = jnp.ones((n_samples, frugal_flow.cond_shape[0]))
+    if (u_yx is None) & (frugal_flow is None):
+        raise ValueError("Either a frugal flow object or u_yx is required")
 
-    uni_standard = jr.uniform(key, shape=(n_samples, flow_dim))
-    uni_minus1_plus1 = jax.vmap(frugal_flow.bijection.bijections[0].tree.transform)(
-        uni_standard
-    )
+    if (u_yx is not None) & (frugal_flow is not None):
+        raise ValueError("Only one between frugal flow object and u_yx can be provided")
 
-    corruni_minus1_plus1 = jax.vmap(frugal_flow.bijection.bijections[1].transform)(
-        uni_minus1_plus1, flow_fake_condition
-    )
-    corruni = jax.vmap(frugal_flow.bijection.bijections[2].tree.transform)(
-        corruni_minus1_plus1, flow_fake_condition
-    )
-    corruni_y = corruni[:, 0]
-    if u_yx != None:
-        assert len(u_yx) == n_samples
-        corruni = u_yx
-    if affine_to_standard:
-        if (corruni_y.min() >= 0.0) & (corruni_y.max() <= 1.0):
-            warnings.warn(
-                "You have activated affine_to_standard but transformed variable was already between 0 and 1"
+    if (causal_model == "location_translation") & (frugal_flow is None):
+        raise ValueError(
+            "A frugal flow object is required for simulating outcome with location_translation model"
+        )
+
+    if frugal_flow is not None:
+        flow_dim = frugal_flow.shape[0]
+
+        if frugal_flow.cond_shape is None:
+            flow_fake_condition = None
+        else:
+            flow_fake_condition = jnp.ones((n_samples, frugal_flow.cond_shape[0]))
+
+        # verify flow has a compatible structure
+
+        assert isinstance(frugal_flow.base_dist, _StandardUniform)
+        assert isinstance(frugal_flow.bijection.bijections[0].tree, Affine)
+        assert isinstance(
+            frugal_flow.bijection.bijections[1].bijection.bijection.bijections[0],
+            MaskedAutoregressiveFirstUniform,
+        )
+
+        maf_dim = (
+            frugal_flow.bijection.bijections[1]
+            .bijection.bijection.bijections[0]
+            .shape[0]
+        )
+        spline_n_params = int(
+            frugal_flow.bijection.bijections[1]
+            .bijection.bijection.bijections[0]
+            .masked_autoregressive_mlp.layers[-1]
+            .out_features
+            / maf_dim
+        )
+        assert (
+            frugal_flow.bijection.bijections[1]
+            .bijection.bijection.bijections[0]
+            .transformer_constructor(jnp.ones((spline_n_params)))
+            .interval
+            == 1
+        )
+        try:
+            assert isinstance(
+                frugal_flow.bijection.bijections[2].tree.bijections[0], Identity
             )
-        corruni_standard = (corruni_y / 2) + 0.5
-    else:
-        corruni_standard = corruni_y
+        except Exception:
+            assert (isinstance(frugal_flow.bijection.bijections[2].tree, Invert)) & (
+                isinstance(frugal_flow.bijection.bijections[2].tree.bijection, Affine)
+            )
+
+        # obtain u_y samples from flow
+        uni_standard = jr.uniform(key, shape=(n_samples, flow_dim))
+        uni_minus1_plus1 = jax.vmap(frugal_flow.bijection.bijections[0].tree.transform)(
+            uni_standard
+        )
+
+        corruni_minus1_plus1 = jax.vmap(frugal_flow.bijection.bijections[1].transform)(
+            uni_minus1_plus1, flow_fake_condition
+        )
+        corruni = jax.vmap(frugal_flow.bijection.bijections[2].tree.transform)(
+            corruni_minus1_plus1, flow_fake_condition
+        )
+        corruni_y = corruni[:, 0]
+
+        try:
+            assert isinstance(
+                frugal_flow.bijection.bijections[2].tree.bijections[0], Identity
+            )
+            corruni_standard = corruni_y
+        except Exception:
+            assert (isinstance(frugal_flow.bijection.bijections[2].tree, Invert)) & (
+                isinstance(frugal_flow.bijection.bijections[2].tree.bijection, Affine)
+            )
+            corruni_standard = (corruni_y / 2) + 0.5
+
+    elif u_yx is not None:
+        assert len(u_yx) == n_samples
+        if (u_yx.min() < 0.0) | (u_yx.max() > 1.0):
+            raise ValueError("u_yx input must be between 0. and 1.")
+        corruni_standard = u_yx
 
     if causal_model == "logistic_regression":
         outcome_samples = logistic_outcome(
-            u_y=corruni_standard, causal_condition=causal_condition, **treatment_kwargs
+            u_y=corruni_standard,
+            causal_condition=causal_condition,
+            **treatment_kwargs,
         )
 
     elif causal_model == "causal_cdf":
         outcome_samples, _ = causal_cdf_outcome(
             u_y=corruni_standard,
             causal_condition=causal_condition,
-            frugal_flow=frugal_flow,
             causal_cdf=causal_cdf,
             **treatment_kwargs,
         )
 
     elif causal_model == "location_translation":
+        try:
+            assert isinstance(
+                frugal_flow.bijection.bijections[4].bijections[0].bijection, Tanh
+            )
+        except Exception:
+            raise ValueError(
+                "location_translation causal_model requires a location_translation pretrained frugal_flow"
+            )
+
         outcome_samples = location_translation_outcome(
             u_y=corruni_standard,
             causal_condition=causal_condition,
@@ -77,7 +153,9 @@ def sample_outcome(
             **treatment_kwargs,
         )
     else:
-        raise ValueError(f"Invalid choice. Please choose from: {valid_causal_models}")
+        raise ValueError(
+            f"Invalid causal_model choice. Please choose from: {valid_causal_models}"
+        )
 
     return outcome_samples
 
@@ -87,7 +165,7 @@ def logistic_outcome(
 ):
     def get_y(u_y, ate, x, const):
         p = jax.nn.sigmoid(ate * x + const)
-        return (u_y >= (1-p)).astype(int)
+        return (u_y >= (1 - p)).astype(int)
 
     return jax.vmap(get_y, in_axes=(0, None, 0, None))(
         u_y, ate, causal_condition, const
@@ -97,7 +175,6 @@ def logistic_outcome(
 def causal_cdf_outcome(
     u_y: ArrayLike,
     causal_cdf: AbstractBijection,
-    frugal_flow: AbstractDistribution,
     causal_condition: ArrayLike,
     **treatment_kwargs: dict,
 ):
@@ -109,16 +186,9 @@ def causal_cdf_outcome(
     for param in causal_cdf_init_params:
         if param not in treatment_kwargs.keys():
             treatment_kwargs[param] = None
-
-    for k, v in treatment_kwargs.items():
-        if k != "cond_dim":
-            if v is None:
-                try:
-                    treatment_kwargs[k] = getattr(
-                        frugal_flow.bijection.bijections[-1].bijection.bijections[0], k
-                    )
-                except Exception:
-                    pass
+            warnings.warn(
+                f"The parameter {param} has not been provided and is therefore set to None."
+            )
 
     causal_cdf_simulate = causal_cdf(**treatment_kwargs)
     samples = jax.vmap(causal_cdf_simulate.inverse)(u_y, causal_condition)
